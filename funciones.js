bbe58d8a21b6e2d1dec3015ab1dabe9cfe6ca669
Alextico 612 <alextico612@gmail.com>
2020-04-29 10:11:03 +0200

    Guardar Cambios

--- funciones.js
+++ funciones.js
@@ -4,8 +4,18 @@ var suma = function suma (a, b) {
 }; 
 
 
+/*Funcion Comprobar cadena */
+var cadena ="Hola /n soy /nAlejandro"
+function ComprobarCadena(cadena )
+{
+    var cadenaAComprobar=cadena.split("/n",3)
+    console.log(cadenaAComprobar);
+    if(cadenaAComprobar[2]=="Alejandro")console.log("El texto es correcto");
+    else console.log("El texto no es correcto")
+}
+ComprobarCadena(cadena);
 
 /* Exportación de funciones */
 module.exports = {
-	suma
+	suma,ComprobarCadena
 }
